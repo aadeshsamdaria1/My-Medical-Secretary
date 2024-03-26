d61788179e1b050ec2fd36b159adc8e28780293f
Latti31 <111042692+Latti31@users.noreply.github.com>
2024-03-26 14:47:41 +1100

    add patient endpoint

--- src/backend/MyMedicalSecretaryBackend/src/main/kotlin/com/mms/mymedicalsecretarybackend/controllers/PatientController.kt
+++ src/backend/MyMedicalSecretaryBackend/src/main/kotlin/com/mms/mymedicalsecretarybackend/controllers/PatientController.kt
@@ -4,34 +4,24 @@ import com.mms.mymedicalsecretarybackend.jpa.entity.PatientEntity
 import com.mms.mymedicalsecretarybackend.jpa.repository.PatientEntityRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.Instant
 
 @RestController
-@RequestMapping("/test")
-class TestController(
+@RequestMapping("/api/patients")
+class PatientController(
     val patientEntityRepository: PatientEntityRepository
 ) {
-    @GetMapping("test1")
-    fun testMapping(): String = "Hello World"
+    @GetMapping("get")
+    fun getPatient(){
 
-    @GetMapping("test2")
-    fun test2(){
-        val x = patientEntityRepository.findById(1)
+    }
 
-        val newPatient = PatientEntity(
-            1,
-            "",
-            "",
-            "",
-            Instant.now(),
-            "",
-            "",
-            "",
-            "",
-        )
+    @PostMapping("create")
+    fun createPatient(@RequestBody patient: PatientEntity): String {
 
-        patientEntityRepository.save(newPatient)
+        patientEntityRepository.save(patient)
+        return "Successfully added new patient!"
     }
 }
