d4789e464e3d98ce78e1c8e7e84c34a404900a07
oli <immortalcy2000@gmail.com>
2024-04-02 13:35:27 +1100

    Reverse changes

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import TabNavigator from './pages/TabNavigator';
-import { StatusBar } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle="dark-content" />
-        <TabNavigator />
-      </SafeAreaView>
-    </GestureHandlerRootView>
+    <>
+      <TabNavigator />
+    </>
   );
 }
