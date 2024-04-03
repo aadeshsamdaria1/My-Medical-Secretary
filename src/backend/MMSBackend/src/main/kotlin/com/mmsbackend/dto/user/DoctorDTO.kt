01b71bfd323b9db8503df6697bea7a967aeee48d
Latti31 <111042692+Latti31@users.noreply.github.com>
2024-04-03 16:00:23 +1100

    add doctor entity and controller

---
+++ src/backend/MMSBackend/src/main/kotlin/com/mmsbackend/dto/user/DoctorDTO.kt
@@ -0,0 +1,10 @@
+package com.mmsbackend.dto.user
+
+data class DoctorDTO (
+    val name: String,
+    val address: String,
+    val contact: String,
+    val email: String,
+    val expertise: String,
+    val website: String
+)
---
+++ src/backend/MMSBackend/src/main/kotlin/com/mmsbackend/mapping/DoctorMapper.kt
@@ -0,0 +1,20 @@
+package com.mmsbackend.mapping
+
+import com.mmsbackend.dto.user.DoctorDTO
+import com.mmsbackend.jpa.entity.DoctorEntity
+import org.springframework.stereotype.Service
+
+@Service
+class DoctorMapper {
+    fun mapDoctorDTO(doctorDTO: DoctorDTO): DoctorEntity {
+        return DoctorEntity(
+            name = doctorDTO.name,
+            address = doctorDTO.address,
+            contact = doctorDTO.contact,
+            email = doctorDTO.email,
+            expertise = doctorDTO.expertise,
+            website = doctorDTO.website,
+            id = 0 // Auto generated
+        )
+    }
+}
