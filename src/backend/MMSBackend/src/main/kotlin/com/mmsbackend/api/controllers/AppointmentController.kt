06c6cc4c3938c7b609ed739edbb510aae40bbb69
Latti31 <111042692+Latti31@users.noreply.github.com>
2024-04-04 17:32:08 +1100

    add get all appointments endpoint

--- src/backend/MMSBackend/src/main/kotlin/com/mmsbackend/api/controllers/AppointmentController.kt
+++ src/backend/MMSBackend/src/main/kotlin/com/mmsbackend/api/controllers/AppointmentController.kt
@@ -3,6 +3,7 @@ package com.mmsbackend.api.controllers
 import com.mmsbackend.dto.appointment.AppointmentDTO
 import com.mmsbackend.jpa.entity.AppointmentEntity
 import com.mmsbackend.jpa.repository.AppointmentEntityRepository
+import com.mmsbackend.jpa.repository.UserEntityRepository
 import com.mmsbackend.mapping.AppointmentMapper
 import org.springframework.web.bind.annotation.*
 import kotlin.jvm.optionals.getOrNull
@@ -10,17 +11,32 @@ import kotlin.jvm.optionals.getOrNull
 @RestController
 @RequestMapping("/api/appointments")
 class AppointmentController (
-        val appointmentEntityRepository: AppointmentEntityRepository,
-        val appointmentMapper: AppointmentMapper
-){
+    val appointmentEntityRepository: AppointmentEntityRepository,
+    val appointmentMapper: AppointmentMapper,
+    val userEntityRepository: UserEntityRepository
+) {
     @GetMapping("/get/{id}")
     fun getAppointment(@PathVariable id: Int): AppointmentEntity? {
         return appointmentEntityRepository.findById(id).getOrNull()
     }
 
+    @GetMapping("/get_all/{userId}")
+    fun getAllAppointmentsForUser(@PathVariable userId: Int): List<AppointmentEntity>? {
+        return appointmentEntityRepository.findAll().filter { appointment ->
+            appointment.user.id == userId
+        }.sortedBy {
+            it.date
+        }
+    }
+
     @PostMapping("/create")
     fun createAppointment(@RequestBody appointmentDTO: AppointmentDTO): String {
-        val appointment = appointmentMapper.mapAppointmentDTO(appointmentDTO)
+        println(appointmentDTO)
+        val appointment = appointmentMapper.mapAppointmentDTO(
+            appointmentDTO = appointmentDTO,
+            userEntity = userEntityRepository.findById(appointmentDTO.userId).get()
+        )
+        println(appointment)
         appointmentEntityRepository.save(appointment)
         return "Successfully added new appointment!"
     }
