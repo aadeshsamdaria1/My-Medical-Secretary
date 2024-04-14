bf27156d2b9c6c87a0e5f335b486c5a757bea1d1
Latti31 <111042692+Latti31@users.noreply.github.com>
2024-04-14 17:58:36 +1000

    add failure test cases

--- src/backend/MMSBackend/src/test/kotlin/com/mmsbackend/service/FileServiceTest.kt
+++ src/backend/MMSBackend/src/test/kotlin/com/mmsbackend/service/FileServiceTest.kt
@@ -3,7 +3,6 @@ package com.mmsbackend.service
 import com.mmsbackend.jpa.entity.AppointmentEntity
 import com.mmsbackend.jpa.entity.DoctorEntity
 import com.mmsbackend.jpa.entity.PatientEntity
-import com.mmsbackend.jpa.entity.UserEntity
 import com.mmsbackend.jpa.repository.AppointmentEntityRepository
 import com.mmsbackend.jpa.repository.DoctorEntityRepository
 import com.mmsbackend.jpa.repository.UserEntityRepository
@@ -12,21 +11,14 @@ import com.mmsbackend.mapping.UserMapper
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
-import io.mockk.slot
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.time.Instant
-import java.time.LocalDate
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 import java.util.*
-import kotlin.random.Random
 
 @ExtendWith(MockKExtension::class)
 class FileServiceTest {
@@ -64,18 +56,26 @@ class FileServiceTest {
         private val userBytes = String(File(filePath, fileName).readBytes())
 
         // Patient ID from HTML
-        private val patientId = 421
+        private val patientId1 = 421
+        private val patientId2 = 4213
 
         @BeforeEach
         fun setup() {
-            every { userEntityRepository.findByPatientId(patientId) } returns null
+            every { userEntityRepository.findByPatientId(patientId1) } returns null
+            every { userEntityRepository.findByPatientId(patientId2) } returns null
             every { userEntityRepository.save( any() ) } answers { invocation.args[0] as PatientEntity }
         }
 
         @Test
         fun `Successfully read and upload a user file`() {
             val userIds = fileService.readAndUploadUserFile(userBytes)
-            assertThat(userIds).isEqualTo(listOf(patientId))
+            assertThat(userIds).isEqualTo(listOf(patientId1, patientId2))
+        }
+
+        @Test
+        fun `Fail to upload corrupted patient data`() {
+            val corruptedUserBytes = userBytes.reversed()
+            assertThrows<Exception> { fileService.readAndUploadUserFile(corruptedUserBytes) }
         }
     }
 
@@ -86,28 +86,58 @@ class FileServiceTest {
         private val appointmentBytes = String(File(filePath, fileName).readBytes())
 
         // IDs from HTML
-        private val appointmentId = 170042
-        private val patientId = 13730
-        private val doctorId = 5
+        private val appointmentId1 = 170042
+        private val appointmentId2 = 1700423
+        private val patientId1 = 13730
+        private val patientId2 = 137303
+        private val doctorId1 = 5
+        private val doctorId2 = 53
+
 
         @MockK
-        private lateinit var patient: PatientEntity
+        private lateinit var patient1: PatientEntity
 
         @MockK
-        private lateinit var doctor: DoctorEntity
+        private lateinit var patient2: PatientEntity
+
+        @MockK
+        private lateinit var doctor1: DoctorEntity
+
+        @MockK
+        private lateinit var doctor2: DoctorEntity
 
         @BeforeEach
         fun setup() {
-            every { appointmentEntityRepository.findById(appointmentId) } returns Optional.empty()
+            every { appointmentEntityRepository.findById(appointmentId1) } returns Optional.empty()
+            every { appointmentEntityRepository.findById(appointmentId2) } returns Optional.empty()
+            every { userEntityRepository.findByPatientId(patientId1) } returns patient1
+            every { userEntityRepository.findByPatientId(patientId2) } returns patient2
+            every { doctorEntityRepository.findById(doctorId1) } returns Optional.of(doctor1)
+            every { doctorEntityRepository.findById(doctorId2) } returns Optional.of(doctor2)
+
             every { appointmentEntityRepository.save( any() ) } answers { invocation.args[0] as AppointmentEntity }
-            every { userEntityRepository.findByPatientId(patientId) } returns patient
-            every { doctorEntityRepository.findById(doctorId) } returns Optional.of(doctor)
         }
 
         @Test
         fun `Successfully read and upload an appointment file`() {
             val appointmentIds = fileService.readAndUploadAppointmentFile(appointmentBytes)
-            assertThat(appointmentIds).isEqualTo(listOf(appointmentId))
+            assertThat(appointmentIds).isEqualTo(listOf(appointmentId1, appointmentId2))
+        }
+
+        @Test
+        fun `Do not upload appointments for which linked patient doesnt exist`() {
+            every { userEntityRepository.findByPatientId(patientId2) } returns null
+
+            val appointmentIds = fileService.readAndUploadAppointmentFile(appointmentBytes)
+            assertThat(appointmentIds).isEqualTo(listOf(appointmentId1))
+        }
+
+        @Test
+        fun `Do not upload appointments for which linked doctor doesnt exist`() {
+            every { doctorEntityRepository.findById(doctorId1) } returns Optional.empty()
+
+            val appointmentIds = fileService.readAndUploadAppointmentFile(appointmentBytes)
+            assertThat(appointmentIds).isEqualTo(listOf(appointmentId2))
         }
     }
 }
