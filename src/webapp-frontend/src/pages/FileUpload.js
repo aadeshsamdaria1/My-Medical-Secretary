c6b481afe6b80f43f9ab77529afda6ddc73c5733
aadeshsamdaria1 <aadeshsamdaria@gmail.com>
2024-04-19 19:24:27 +1000

    creating basic layout

---
+++ src/webapp-frontend/src/pages/FileUpload.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+function FileUpload() {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <div>File Upload</div>
+        </div>
+    )
+}
+
+export default FileUpload
