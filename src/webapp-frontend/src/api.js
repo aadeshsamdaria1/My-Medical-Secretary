c3c8d5ef9f4848558273ea4d542b4aca85fb3a2e
Latti31 <111042692+Latti31@users.noreply.github.com>
2024-05-13 12:06:14 +1000

    move to https

--- src/webapp-frontend/src/api.js
+++ src/webapp-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_ENDPOINT = 'http://wombat-mms.ap-southeast-2.elasticbeanstalk.com:8080/api';
+export const API_ENDPOINT = 'https://wombat-mms.ap-southeast-2.elasticbeanstalk.com:8080/api';
 
 // API endpoints
 export const loginEndpoint = `${API_ENDPOINT}/login`;
