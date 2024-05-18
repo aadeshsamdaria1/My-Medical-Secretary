05d1e6c68bfab12bfec09acf930fab39ff5e16d3
Sophie von Doussa <svondoussa@student.unimelb.edu.au>
2024-05-18 15:15:30 +1000

    Create admin API functions

---
+++ src/webapp-frontend/src/utils/adminAccountsAPI.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import {
+    getAllAdminsEndpoint,
+    createAdminEndpoint,
+    deleteAdminByIdEndpoint
+} from "../api"
+
+  export const getAllAdmins = async () => {
+    try {
+      const jwtToken = localStorage.getItem('jwtToken');
+      const response = await axios.get(getAllAdminsEndpoint, {
+        headers: { Authorization: `Bearer ${jwtToken}` }
+      });
+      return response.data;
+    } catch (error) {
+      return handleRequestError(error, axios.get, getAllAdminsEndpoint);
+    }
+  };
+  
+  export const createAdmin = async (adminData) => {
+    try {
+      const jwtToken = localStorage.getItem('jwtToken');
+      const response = await axios.post(createAdminEndpoint, adminData, {
+        headers: { Authorization: `Bearer ${jwtToken}` }
+      });
+      return response.data;
+    } catch (error) {
+      return handleRequestError(error, axios.post, createAdminEndpoint, adminData);
+    }
+  };
+  
+
+
+export const deleteAdminById = async (id) => {
+    try {
+      const jwtToken = localStorage.getItem('jwtToken');
+      const response = await axios.delete(`${deleteAdminByIdEndpoint}/${id}`, {
+        headers: { Authorization: `Bearer ${jwtToken}` }
+      });
+      return response.data;
+    } catch (error) {
+      return handleRequestError(error, axios.delete, `${deleteAdminByIdEndpoint}/${id}`);
+    }
+  };
+  
+
+
+  
