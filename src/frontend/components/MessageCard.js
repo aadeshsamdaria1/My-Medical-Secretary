19ed313a223c196013d3ebc618d4aad8c60accf8
Sophie von Doussa <svondoussa@student.unimelb.edu.au>
2024-05-23 11:15:02 +1000

    Fixed message card UI on android

--- src/frontend/components/MessageCard.js
+++ src/frontend/components/MessageCard.js
@@ -50,7 +50,6 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 5,
     borderWidth: 1,
     borderColor: "#DDD",
   },
